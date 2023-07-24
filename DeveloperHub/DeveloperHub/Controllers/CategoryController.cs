843c12e4e4c3f102c1e785f3f8b7730b363cf26d
Aldrin1807 <ai58914@ubt-uni.net>
2023-07-24 21:38:18 +0200

    Added categories view and controller

---
+++ DeveloperHub/DeveloperHub/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperHub.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+                    _context = context;
+        }   
+
+        public async Task<IActionResult> Index()
+        {
+            var categories =await _context.Categories
+                .ToListAsync();
+            foreach (var category in categories)
+            {
+                category.topicCount = _context.Topics.Count(t => t.CategoryID == category.Id);
+            }
+
+            return View(categories);
+        }
+    }
+}
--- DeveloperHub/DeveloperHub/Models/Category.cs
+++ DeveloperHub/DeveloperHub/Models/Category.cs
@@ -1,11 +1,14 @@
-﻿namespace DeveloperHub.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeveloperHub.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        
+        [NotMapped]
+        public int topicCount { get; set; }
 
 
         public List<Topic>  Topics { get; set; }
